66597109ff04245006bbef2a27389bc30b5e3e27
Vegard Stronen <70795294+stronenv@users.noreply.github.com>
2025-05-04 10:09:31 +0200

    Update terrassebordkalkulator.php

--- wp-content/plugins/terrassebordkalkulator/terrassebordkalkulator.php
+++ wp-content/plugins/terrassebordkalkulator/terrassebordkalkulator.php
@@ -4,8 +4,8 @@
  * Description:       Block for Terrassebordkalkulator
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           0.1.1
+ * Author:            stronenv
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       terrassebordkalkulator
